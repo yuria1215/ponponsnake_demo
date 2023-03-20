641dc606fe6006ac07a5e6e57a7529b2669f1758
Reila W <ravennreila@gmail.com>
2023-03-20 09:34:01 +0800

    test

--- components/Join.js
+++ components/Join.js
@@ -15,9 +15,46 @@ export default function Join() {
 
                 <div className="join-info-block">
 
-                    <img className="join-info" src={`${process.env.BASE_PATH}/images/join/text-01.png`} alt="" />
+                    {/* <img className="join-info" src={`${process.env.BASE_PATH}/images/join/text-01.png`} alt="" />
                     <img className="join-info" src={`${process.env.BASE_PATH}/images/join/text-02.png`} alt="" />
-                    <img className="join-info" src={`${process.env.BASE_PATH}/images/join/text-03.png`} alt="" />
+                    <img className="join-info" src={`${process.env.BASE_PATH}/images/join/text-03.png`} alt="" /> */}
+
+                    <div className="join-info-BG">
+                        <img className="info-BG" src={`${process.env.BASE_PATH}/images/join/join-info-BG.png`} alt="" />
+                        <div className="join-info-inner">
+                            <div className="join-info-title">Stabilize</div>
+                            <div className="info-title-border"></div>
+                            <div className="join-info-text">
+                                PonPonSnake is a stable company, we have been in the game industry for many years and have rich experience. We are also one of the few Taiwan companies developing into GameFi.
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <div className="join-info-BG">
+                        <img className="info-BG" src={`${process.env.BASE_PATH}/images/join/join-info-BG.png`} alt="" />
+                        <div className="join-info-inner">
+                            <div className="join-info-title">Professional</div>
+                            <div className="info-title-border"></div>
+                            <div className="join-info-text">
+                                PonPonSnake has the most professional team, including design, art, planning, and programming, which can transform your brand NFT with the most professional attitude and expertise.
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="join-info-BG">
+                        <img className="info-BG" src={`${process.env.BASE_PATH}/images/join/join-info-BG.png`} alt="" />
+                        <div className="join-info-inner">
+                            <div className="join-info-title">Variability</div>
+                            <div className="info-title-border"></div>
+                            <div className="join-info-text">
+                                We may offer a range of choices, from characters to lands. As more and more unique NFT universes appear, we will maintain the pace and avoid falling behind.
+                            </div>
+
+                        </div>
+                    </div>
 
                 </div>
 
