5a51ecdc5f685ff3867c574d06bd75ec274eac80
yuria1215 <takaii666@gmail.com>
2023-04-15 00:14:08 +0800

    script jquery

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,3 @@
-// import '@/styles/index.css'
-// import '@/styles/banner.css'
-// import '@/styles/arena-events.css'
 import '@/styles/style.scss'
 
 export default function App({ Component, pageProps }) {
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -7,6 +8,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"
+          strategy="beforeInteractive"
+        />
       </body>
     </Html>
   )
