3a302efcb7f448e8b6b09340cf7264b729d64609
yuria1215 <takaii666@gmail.com>
2023-04-28 15:06:56 +0800

    ContactPopup

--- components/layout/ContactPopup.js
+++ components/layout/ContactPopup.js
@@ -28,6 +28,109 @@ export default function ContactPopup() {
     // }, []);
 
 
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [title, setTitle] = useState('');
+    const [message, setMessage] = useState('');
+    const [errors, setErrors] = useState({});
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // 表單驗證
+        // const errors = {};
+
+        // if (!name) {
+        //     errors.name = '請填寫姓名';
+        // }
+        // if (!email) {
+        //     errors.email = '請填寫 Email';
+        // }
+        // if (!message) {
+        //     errors.message = '請填寫訊息';
+        // }
+        // if (Object.keys(errors).length > 0) {
+        //     setErrors(errors);
+        //     return;
+        // }
+
+        // const handleInputChange = (e) => {
+        //     const { name, value } = e.target;
+
+        //     // 清除與該輸入欄位相關聯的錯誤訊息
+        //     setErrors((prevErrors) => ({ ...prevErrors, [name]: '' }));
+
+        //     // 更新輸入欄位的值
+        //     if (name === 'name') {
+        //         setName(value);
+        //     } else if (name === 'email') {
+        //         setEmail(value);
+        //     } else if (name === 'message') {
+        //         setMessage(value);
+        //     }
+        // };
+
+
+
+        var formData = `{"name-input":"${name}","email-input":"${email}","phone-input":"${phone}","title-input":"${title},"message-input":"${message}"}`
+
+        fetch('https://feedback.cqiserv.com/feedback/official/cqigames', {
+            method: "POST",
+
+            crossDomain: true,
+            mode: 'cors',
+            redirect: 'follow',
+            body: formData,
+            headers: new Headers({
+                'Content-Type': 'application/json;charset=UTF-8',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, HEAD'
+            }),
+            // mode: 'no-cors',
+        })
+            .then((response) => { return response.json(); })
+            .then(function (response) {
+
+                // console.log(response)
+                if (true) {
+                    cleanData();
+                    alert("感謝您的回饋，我們會持續努力做出好遊戲");
+                } else {
+                    alert("請確認網路是否連線");
+                }
+            }).catch(function (err) {
+                console.log(err);
+                alert("請確認網路是否連線!");
+            })
+
+
+
+
+
+        // 將表單資料送到後端
+
+        // 清空表單資料
+        const cleanData = () => {
+            setName('');
+            setEmail('');
+            setPhone('');
+            setTitle('');
+            setMessage('');
+            setErrors('');
+        }
+
+
+
+        console.log(formData);
+
+        // 點擊contact-popup以外的地方彈窗關閉
+    };
+
+
+
     return (
         <div className="contactPopupBlock">
 
@@ -53,54 +156,60 @@ export default function ContactPopup() {
             </div>
 
             <div className={`contact-popup ${showContactPopup ? "showContactPopup" : ""}`}>
-                    <div className="contact-popup-inner">
-                        <div className="contact-title">
-                            Contact Us
-                        </div>
-
-                        <div className="input-data">
-
-                            <div className="form-group">
-                                <input type="text" id="name-input" className="input-form-control" placeholder=" " />
-                                <label htmlFor="name-input">Your Name</label>
-                            </div>
-
-                            <div className="form-group">
-                                <input type="text" id="email-input" className="input-form-control" placeholder=" " />
-                                <label htmlFor="email-input">Your Email</label>
-                            </div>
-
-                            <div className="form-group">
-                                <input type="text" id="phone-input" className="input-form-control" placeholder=" " />
-                                <label htmlFor="phone-input">Your Phone</label>
-                            </div>
-
-                            <div className="form-group">
-                                <input type="text" id="title-input" className="input-form-control" placeholder=" " />
-                                <label htmlFor="title-input">Feedback Title</label>
-                            </div>
-
-                            <div className="form-group">
-                                <textarea id="message-textarea" className="textarea-form-control" placeholder=" " />
-                                <label htmlFor="message-textarea">What's About</label>
-                            </div>
-
-
-                            <button type="submit" className="submit-button">
-                                Submit
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16.08" height="16.08" viewBox="0 0 16.08 16.08">
-                                    <path id="send" d="M15.828.1a.561.561,0,0,1,.242.574l-2.3,13.783a.564.564,0,0,1-.287.4.55.55,0,0,1-.278.072.606.606,0,0,1-.215-.045l-4.065-1.66L6.755,15.874a.529.529,0,0,1-.44.206.487.487,0,0,1-.2-.036.574.574,0,0,1-.377-.539V12.374l7.753-9.5L3.9,11.172.358,9.718A.568.568,0,0,1,.286,8.7L15.217.081A.548.548,0,0,1,15.5,0a.556.556,0,0,1,.323.1Z" transform="translate(0.002)" fill="#343456" />
-                                </svg>
-
-
-                            </button>
-
-                        </div>
-
-
-
+                <div className="contact-popup-inner">
+                    <div className="contact-title">
+                        Contact Us
                     </div>
+
+                    <form className="input-data" onSubmit={handleSubmit}>
+
+                        <div className="form-group">
+                            <input type="text" id="name-input" className="input-form-control"
+                                value={name} onChange={(e) => setName(e.target.value)} placeholder=" " />
+                            <label htmlFor="name-input">Your Name</label>
+                            {/* {errors.name && <div className="error">{errors.name}</div>} */}
+                        </div>
+
+                        <div className="form-group">
+                            <input type="text" id="email-input" className="input-form-control"
+                                value={email} onChange={(e) => setEmail(e.target.value)} placeholder=" " />
+                            <label htmlFor="email-input">Your Email</label>
+                        </div>
+
+                        <div className="form-group">
+                            <input type="text" id="phone-input" className="input-form-control"
+                                value={phone} onChange={(e) => setPhone(e.target.value)} placeholder=" " />
+                            <label htmlFor="phone-input">Your Phone</label>
+                        </div>
+
+                        <div className="form-group">
+                            <input type="text" id="title-input" className="input-form-control"
+                                value={title} onChange={(e) => setTitle(e.target.value)} placeholder=" " />
+                            <label htmlFor="title-input">Feedback Title</label>
+                        </div>
+
+                        <div className="form-group">
+                            <textarea id="message-input" className="textarea-form-control"
+                                value={message} onChange={(e) => setMessage(e.target.value)} placeholder=" " />
+                            <label htmlFor="message-input">What's About</label>
+                        </div>
+
+
+                        <button type="submit" className="submit-button">
+                            Submit
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16.08" height="16.08" viewBox="0 0 16.08 16.08">
+                                <path id="send" d="M15.828.1a.561.561,0,0,1,.242.574l-2.3,13.783a.564.564,0,0,1-.287.4.55.55,0,0,1-.278.072.606.606,0,0,1-.215-.045l-4.065-1.66L6.755,15.874a.529.529,0,0,1-.44.206.487.487,0,0,1-.2-.036.574.574,0,0,1-.377-.539V12.374l7.753-9.5L3.9,11.172.358,9.718A.568.568,0,0,1,.286,8.7L15.217.081A.548.548,0,0,1,15.5,0a.556.556,0,0,1,.323.1Z" transform="translate(0.002)" fill="#343456" />
+                            </svg>
+
+
+                        </button>
+
+                    </form>
+
+
+
                 </div>
+            </div>
 
 
 
